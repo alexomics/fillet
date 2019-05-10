d322b34741b63a05669bdb5d4278ed00ea5d6940
Alex Payne <alexander.payne@nottingham.ac.uk>
2019-05-10 14:43:56 +0100

    Updated license

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     python_requires=">=3.5",
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
     entry_points={
